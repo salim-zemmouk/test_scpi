50a3bedb789983976117eb1444e1fe54e3cfa07d
salim zemmouk <salimzemmouk@gmail.com>
2025-05-09 01:52:49 +0200

    update jenkinsfile fix 1

--- scripts/posttest.sh
+++ scripts/posttest.sh
@@ -4,7 +4,7 @@
 npm install --save-dev mochawesome mochawesome-merge mochawesome-report-generator
 
 # Fusionner tous les fichiers JSON générés par Cypress
-npx mochawesome-merge cypress/reports/html/jsons/*.json > cypress/reports/html/mochawesome.json
+npx mochawesome-merge cypress/reports/html/.jsons/*.json > cypress/reports/html/mochawesome.json
 
 # Générer un rapport HTML à partir du JSON fusionné
 npx marge cypress/reports/html/mochawesome.json --reportDir cypress/reports/html --reportFilename index
